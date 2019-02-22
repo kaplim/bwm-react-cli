dc641bee8df284f35cecf6094928b5fdc094e1d4
Kap Lim <freekaplim@gmail.com>
2019-02-22 12:31:43 -0500

    Add predeploy setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const path = require('path');
+
+const config = require('./config');
 const FakeDb = require('./fake-db');
 
 const rentalRoutes = require('./routes/rentals');
@@ -9,8 +11,10 @@ const userRoutes = require('./routes/users');
 const bookingRoutes = require('./routes/bookings');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
-	const fakeDb = new FakeDb();
-	//fakeDb.seedDb();
+	if (process.env.NODE_ENV !== 'production') {
+		const fakeDb = new FakeDb();
+		//fakeDb.seedDb();
+	}
 });
 
 const app = express();
@@ -20,6 +24,15 @@ app.use(bodyParser.json());
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+	const appPath = path.join(__dirname, '..', 'build');
+	app.use(express.static(appPath));
+
+	app.get('*', function(req, res) {
+		res.sendFile(path.resolve(appPath, 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 3001;
 
